605aa5b5e88e5901c108c06474472af132e80ce7
inba <144317561+Inba0007@users.noreply.github.com>
2025-03-11 09:33:50 +0530

    Add files via upload

---
+++ PrintReverseNumbers.java
@@ -0,0 +1,13 @@
+public class PrintReverseNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-gen>erated method stub
+		for (int i=10; i>=1; i--) {
+			System.out.println(i);
+			
+		
+		}
+
+	}
+
+}
